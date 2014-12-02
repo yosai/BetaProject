fce2fed079122d18d3de1666d21671c188a48d91
yosai <youssef.saih@ymail.com>
2014-12-02 09:54:20 +0100

    ajout ficher sql

---
+++ creationTable.sql
@@ -0,0 +1,114 @@
+CREATE TABLE joueursEchec (
+    nom VARCHAR(50),
+    prenom VARCHAR(50),
+    pays VARCHAR(50) NOT NULL,
+    score INT NOT NULL,
+    naissance INT NOT NULL,
+    PRIMARY KEY (nom,prenom)
+);
+
+
+
+
+INSERT INTO joueursEchec VALUES ('Carlsen','Magnus','Norvège',2870,1990);
+INSERT INTO joueursEchec VALUES ('Aronian','Levon','Arménie',2804,1982);
+INSERT INTO joueursEchec VALUES ('Caruana','Fabiano','Italie',2801,1992);
+INSERT INTO joueursEchec VALUES ('Grischuk','Alexander','Russie',2789,1983);
+INSERT INTO joueursEchec VALUES ('Anand','Viswanathan','Inde',2785,1969);
+INSERT INTO joueursEchec VALUES ('Topalov','Veselin','Bulgarie',2784,1975);
+INSERT INTO joueursEchec VALUES ('Nakamura','Hikaru','USA',2782,1987);
+INSERT INTO joueursEchec VALUES ('Karjakin','Sergey','Russie',2777,1990);
+INSERT INTO joueursEchec VALUES ('Vachier-Lagrave','Maxime','France',2768,1990);
+INSERT INTO joueursEchec VALUES ('Kramnik','Vladimir','Russie',2760,1975);
+INSERT INTO joueursEchec VALUES ('Giri','Anish','Pays-Bas',2758,1994);
+INSERT INTO joueursEchec VALUES ('Mamedyarov','Shakhriyar','Azerbaïdjan',2756,1985);
+INSERT INTO joueursEchec VALUES ('Dominguez','Leinier','Cuba',2755,1983);
+INSERT INTO joueursEchec VALUES ('So','Wesley','Philippines',2755,1993);
+INSERT INTO joueursEchec VALUES ('Ding','Liren','Chine',2754,1992);
+INSERT INTO joueursEchec VALUES ('Adams','Michael','Angleterre',2752,1971);
+INSERT INTO joueursEchec VALUES ('Gelfand','Boris','Israël',2748,1968);
+INSERT INTO joueursEchec VALUES ('Jakovenko','Dmitry','Russie',2747,1983);
+INSERT INTO joueursEchec VALUES ('Vitiugov','Nikita','Russie',2742,1987);
+INSERT INTO joueursEchec VALUES ('Wojtaszek','Radoslaw','Pologne',2736,1987);
+INSERT INTO joueursEchec VALUES ('Leko','Peter','Hongrie',2734,1979);
+INSERT INTO joueursEchec VALUES ('Svidler','Peter','Russie',2732,1976);
+INSERT INTO joueursEchec VALUES ('Morozevich','Alexander','Russie',2731,1977);
+INSERT INTO joueursEchec VALUES ('Wang','Hao','Chine',2730,1989);
+INSERT INTO joueursEchec VALUES ('Bacrot','Etienne','France',2729,1983);
+INSERT INTO joueursEchec VALUES ('Eljanov','Pavel','Ukraine',2726,1983);
+INSERT INTO joueursEchec VALUES ('Ivanchuk','Vassily','Ukraine',2726,1969);
+INSERT INTO joueursEchec VALUES ('Harikrishna','Pentala','Inde',2725,1986);
+INSERT INTO joueursEchec VALUES ('Bu','Xiangzhi','Chine',2722,1985);
+INSERT INTO joueursEchec VALUES ('Navara','David','République tchèque',2722,1985);
+INSERT INTO joueursEchec VALUES ('Andreikin','Dmitry','Russie',2722,1990);
+INSERT INTO joueursEchec VALUES ('Ponomariov','Ruslan','Ukraine',2717,1983);
+INSERT INTO joueursEchec VALUES ('Radjabov','Teimour','Azerbaïdjan',2717,1987);
+INSERT INTO joueursEchec VALUES ('Naiditsch','Arkadij','Allemagne',2715,1985);
+INSERT INTO joueursEchec VALUES ('Rapport','Richard','Hongrie',2713,1996);
+INSERT INTO joueursEchec VALUES ('Wang','Yue','Chine',2713,1987);
+INSERT INTO joueursEchec VALUES ('Fressinet','Laurent','France',2712,1981);
+INSERT INTO joueursEchec VALUES ('Vallejo Pons','Francisco','Espagne',2712,1982);
+INSERT INTO joueursEchec VALUES ('Li','Chao b','Chine',2711,1989);
+INSERT INTO joueursEchec VALUES ('Nepomniachtchi','Ian','Russie',2710,1990);
+INSERT INTO joueursEchec VALUES ('Jobava','Baadur','Géorgie',2708,1983);
+INSERT INTO joueursEchec VALUES ('Kryvoruchko','Yuriy','Ukraine',2708,1986);
+INSERT INTO joueursEchec VALUES ('Kasimdzhanov','Rustam','Ouzbékistan',2706,1979);
+INSERT INTO joueursEchec VALUES ('Le','Quang Liem','Vietnam',2706,1991);
+INSERT INTO joueursEchec VALUES ('Shirov','Alexei','Lettonie',2701,1972);
+INSERT INTO joueursEchec VALUES ('Tomashevsky','Evgeny','Russie',2701,1987);
+INSERT INTO joueursEchec VALUES ('Yu','Yangyi','Chine',2700,1994);
+INSERT INTO joueursEchec VALUES ('Moiseenko','Alexander','Ukraine',2698,1980);
+INSERT INTO joueursEchec VALUES ('Rublevsky','Sergei','Russie',2698,1974);
+INSERT INTO joueursEchec VALUES ('Malakhov','Vladimir','Russie',2696,1980);
+INSERT INTO joueursEchec VALUES ('Matlakov','Maxim','Russie',2694,1991);
+INSERT INTO joueursEchec VALUES ('Almasi','Zoltan','Hongrie',2694,1976);
+INSERT INTO joueursEchec VALUES ('Sargissian','Gabriel','Arménie',2691,1983);
+INSERT INTO joueursEchec VALUES ('Motylev','Alexander','Russie',2686,1979);
+INSERT INTO joueursEchec VALUES ('Lysyj','Igor','Russie',2684,1987);
+INSERT INTO joueursEchec VALUES ('Kamsky','Gata','USA',2681,1974);
+INSERT INTO joueursEchec VALUES ('Khismatullin','Denis','Russie',2681,1984);
+INSERT INTO joueursEchec VALUES ('Sasikiran','Krishnan','Inde',2680,1981);
+INSERT INTO joueursEchec VALUES ('Bruzon Batista','Lazaro','Cuba',2679,1982);
+INSERT INTO joueursEchec VALUES ('Rodshtein','Maxim','Israël',2678,1989);
+INSERT INTO joueursEchec VALUES ('Melkumyan','Hrant','Arménie',2678,1989);
+INSERT INTO joueursEchec VALUES ('Saric','Ivan','Croatie',2678,1990);
+INSERT INTO joueursEchec VALUES ('Riazantsev','Alexander','Russie',2678,1985);
+INSERT INTO joueursEchec VALUES ('Zhigalko','Sergei','Biélorussie',2677,1989);
+INSERT INTO joueursEchec VALUES ('Fedoseev','Vladimir','Russie',2677,1995);
+INSERT INTO joueursEchec VALUES ('Granda Zuniga','Julio E','Pérou',2675,1967);
+INSERT INTO joueursEchec VALUES ('Laznicka','Viktor','République tchèque',2675,1988);
+INSERT INTO joueursEchec VALUES ('Ni','Hua','Chine',2675,1983);
+INSERT INTO joueursEchec VALUES ('Polgar','Judit','Hongrie',2675,1976);
+INSERT INTO joueursEchec VALUES ('Alekseev','Evgeny','Russie',2675,1985);
+INSERT INTO joueursEchec VALUES ('Edouard','Romain','France',2674,1990);
+INSERT INTO joueursEchec VALUES ('Cheparinov','Ivan','Bulgarie',2674,1986);
+INSERT INTO joueursEchec VALUES ('Grachev','Boris','Russie',2673,1986);
+INSERT INTO joueursEchec VALUES ('Korobov','Anton','Ukraine',2673,1985);
+INSERT INTO joueursEchec VALUES ('Nisipeanu','Liviu-Dieter','Allemagne',2672,1976);
+INSERT INTO joueursEchec VALUES ('Inarkiev','Ernesto','Russie',2672,1985);
+INSERT INTO joueursEchec VALUES ('Landa','Konstantin','Russie',2670,1972);
+INSERT INTO joueursEchec VALUES ('McShane','Luke','Angleterre',2670,1984);
+INSERT INTO joueursEchec VALUES ('Negi','Parimarjan','Inde',2669,1993);
+INSERT INTO joueursEchec VALUES ('Tiviakov','Sergei','Pays-Bas',2668,1973);
+INSERT INTO joueursEchec VALUES ('Fedorchuk','Sergey','Ukraine',2665,1981);
+INSERT INTO joueursEchec VALUES ('Onischuk','Alexander','USA',2665,1975);
+INSERT INTO joueursEchec VALUES ('Jones','Gawain','Angleterre',2664,1987);
+INSERT INTO joueursEchec VALUES ('Sjugirov','Sanan','Russie',2664,1993);
+INSERT INTO joueursEchec VALUES ('Areshchenko','Alexander','Ukraine',2664,1986);
+INSERT INTO joueursEchec VALUES ('Movsesian','Sergei','Arménie',2663,1978);
+INSERT INTO joueursEchec VALUES ('Hou','Yifan','Chine',2663,1994);
+INSERT INTO joueursEchec VALUES ('Kuzubov','Yuriy','Ukraine',2663,1990);
+INSERT INTO joueursEchec VALUES ('Dreev','Aleksey','Russie',2662,1969);
+INSERT INTO joueursEchec VALUES ('Iturrizaga Bonelli','Eduardo','Vénézuela',2660,1989);
+INSERT INTO joueursEchec VALUES ('Balogh','Csaba','Hongrie',2660,1987);
+INSERT INTO joueursEchec VALUES ('Delchev','Aleksander','Bulgarie',2660,1971);
+INSERT INTO joueursEchec VALUES ('Mamedov','Rauf','Azerbaïdjan',2660,1988);
+INSERT INTO joueursEchec VALUES ('Nguyen','Ngoc Truong Son','Vietnam',2659,1990);
+INSERT INTO joueursEchec VALUES ('Zvjaginsev','Vadim','Russie',2659,1976);
+INSERT INTO joueursEchec VALUES ('Howell','David','Angleterre',2657,1990);
+INSERT INTO joueursEchec VALUES ('Akopian','Vladimir','Arménie',2657,1971);
+INSERT INTO joueursEchec VALUES ('Gharamian','Tigran','France',2657,1984);
+INSERT INTO joueursEchec VALUES ('Ragger','Markus','Autriche',2657,1988);
+INSERT INTO joueursEchec VALUES ('Smirin','Ilia','Israël',2655,1968);
+INSERT INTO joueursEchec VALUES ('Safarli','Eltaj','Azerbaïdjan',2655,1992);
+INSERT INTO joueursEchec VALUES ('Baklan','Vladimir','Ukraine',2655,1978);
